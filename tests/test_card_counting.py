04698bd1d5d52c7b9e04bf2a0a613c0176882014
Spyros Babalitis <spyridon.babalitis@students.plymouth.ac.uk>
2024-04-05 19:23:59 +0100

    Fix: integration test WIP

--- tests/test_card_counting.py
+++ tests/test_card_counting.py
@@ -224,7 +224,85 @@ def test_buying_settlement_transaction_is_tracked_using_unknown():
     assert CardCounting_Blue.assumed_resources[Color.RED]['unknown_list'] == []
 
 def test_buying_settlement_transaction_is_tracked_using_unknown_against_state():
-    pass
+    players = [SimplePlayer(Color.RED), SimplePlayer(Color.BLUE)]
+    game = Game(players)
+    # map setup
+    for tile in game.state.board.map.land_tiles.values():
+        if tile.id == 0:
+            tile.resource = WOOD
+            tile.number = 8
+
+        elif tile.id == 1:
+            tile.resource = WHEAT
+            tile.number = 5
+        elif tile.id == 2:
+            tile.resource = SHEEP
+            tile.number = 5
+        
+        elif tile.id == 6:
+            tile.resource = BRICK
+            tile.number = 8
+        
+        elif tile.id == 7:
+            tile.resource = ORE
+            tile.number = 4
+        
+        elif tile.id == 8:
+            tile.resource = SHEEP
+            tile.number = 6
+        
+        elif tile.id == 9:
+            tile.resource = ORE
+            tile.number = 6
+        else:
+            tile.number = 2
+
+    game.state.board.build_settlement(Color.RED, 1, initial_build_phase=True)
+    game.state.board.build_road(Color.RED, (1, 2))
+    game.state.board.build_settlement(Color.BLUE, 26, initial_build_phase=True)
+    game.state.board.build_road(Color.BLUE, (26, 27))
+    game.state.board.build_settlement(Color.BLUE, 28, initial_build_phase=True)
+    game.state.board.build_road(Color.BLUE, (27, 28))
+    game.state.board.build_settlement(Color.RED, 9, initial_build_phase=True)
+    game.state.board.build_road(Color.RED, (9, 10))
+    # red = sheep, brick, ore
+    # blue = BRICK
+
+
+    # 1 wood, 2 brick, 1 wheat, 1 sheep, 1 ore
+    player_freqdeck_add(game.state, Color.RED, [1, 2, 1, 1, 1])
+    
+    CardCounting_Blue = CardCounting(game, Color.BLUE)
+    # 1 wood, 2 brick, 0 wheat, 0 sheep, 1 ore, 2 unknown
+    CardCounting_Blue.assumed_resources[Color.RED][WOOD] = 1
+    CardCounting_Blue.assumed_resources[Color.RED][BRICK] = 2
+    CardCounting_Blue.assumed_resources[Color.RED][SHEEP] = 0
+    CardCounting_Blue.assumed_resources[Color.RED][WHEAT] = 0
+    CardCounting_Blue.assumed_resources[Color.RED][ORE] = 1
+    CardCounting_Blue.assumed_resources[Color.RED][UNKNOWN] = 2
+    CardCounting_Blue.assumed_resources[Color.RED]['unknown_list'] = [SHEEP, WHEAT]
+
+
+    action_roll = Action(Color.RED, ActionType.ROLL, (6, 6))
+    apply_action(game.state, action=action_roll)
+    action = Action(Color.RED, ActionType.BUILD_SETTLEMENT, 4)
+    apply_action(game.state, action=action)
+
+    CardCounting_Blue.update_opponent_resources(game.state, action)
+
+    assert player_num_resource_cards(game.state, Color.RED, WOOD) == 0
+    assert player_num_resource_cards(game.state, Color.RED, BRICK) == 1
+    assert player_num_resource_cards(game.state, Color.RED, SHEEP) == 0
+    assert player_num_resource_cards(game.state, Color.RED, WHEAT) == 0
+    assert player_num_resource_cards(game.state, Color.RED, ORE) == 1
+
+    assert CardCounting_Blue.assumed_resources[Color.RED][WOOD] == 0
+    assert CardCounting_Blue.assumed_resources[Color.RED][BRICK] == 1
+    assert CardCounting_Blue.assumed_resources[Color.RED][SHEEP] == 0
+    assert CardCounting_Blue.assumed_resources[Color.RED][WHEAT] == 0
+    assert CardCounting_Blue.assumed_resources[Color.RED][ORE] == 1
+    assert CardCounting_Blue.assumed_resources[Color.RED][UNKNOWN] == 0
+    assert CardCounting_Blue.assumed_resources[Color.RED]['unknown_list'] == []
 
 def test_buying_city_transaction_is_tracked_using_assumed_against_state():
     # needs work
